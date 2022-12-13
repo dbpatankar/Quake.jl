81638067d74fddb6326e9fd19236fb5f3984ed02
Digvijay Patankar <dbpatankar@gmail.com>
2022-12-13 12:27:38 +0530

    comments + try, catch in peernga parser

--- src/Quake.jl
+++ src/Quake.jl
@@ -2,6 +2,7 @@ module Quake
 
 # Write your package code here.
 #
+export read_peernga_file, read_cosmosvdc_file
 include("SignalParsers.jl")
 
 end
--- src/SignalParsers.jl
+++ src/SignalParsers.jl
@@ -20,23 +20,41 @@ Reads a PeerNGA format file and returns a TimeSeries struct.
 # Returns
 TimeSeries struct
 """
-function read_peernga_file(filename, skiprows=4)
+function read_peernga_file(filename::String, skiprows=4)
     start = skiprows + 1
     headRows = skiprows
     lines = readlines(filename)
-    event, date, station, comp = [strip(i) for i in split(lines[2], ",")]
+
+    # parse line 2
+    try
+        event, date, station, comp = [strip(i) for i in split(lines[2], ",")]
+    catch
+        println("Unable to parse event, date, station and component on line 2")
+        event, date, station, comp = [" " for i = 1:4]
+    end
+
+
+    # parse lines for yunit
     yunitVec = [match(r"(.*TIME SERIES IN UNITS OF.*)", lines[i]) for i = 1:headRows]
-    yunitline = yunitVec[yunitVec .!= nothing][1]
-    yunit = yunitline[1]
+    yunitline = yunitVec[yunitVec .!= nothing]
+    yunit = length(yunitline) == 0 ? " " : yunitline[1]
+
+    # parse lines for npts and dt
     ptslineVec = [match(r"\s*[NPTSnpts ]*=?\s*(\d*).*[DTdt ]+=?\s*([\.0-9]+)\s*SEC", lines[i]) for i=1:headRows]
-    ptsline = ptslineVec[ptslineVec .!= nothing][1]
-    npts, dt = ptsline
+    ptsline = ptslineVec[ptslineVec .!= nothing]
+    npts, dt = length(ptsline) == 0 ? ["-9", "-1"] : ptsline[1]
     npts = parse(Int64, npts)
     dt = parse(Float64, dt)
+
     # Get the data
-    y = [parse(Float64, j) for i = start:length(lines) for j in split(lines[i])]
+    try
+        y = [parse(Float64, j) for i = start:length(lines) for j in split(lines[i])]
+    catch e
+        println("Unable to read data from file.")
+        throw(e)
+    end
     t = dt:dt:npts*dt
-    #return TimeSeries(filename, t, y, dt, npts, yunit, t[end], event, date, station, comp, " ")
+
     meta = Dict(
         "dt" => dt,
         "npts" => npts,
@@ -63,50 +81,70 @@ end
 # Returns
 three TimeSeries structs for acceleration, velocity and displacement, respectively.
 """
-function read_cosmosvdc_file(filename, skiprows=6)
+function read_cosmosvdc_file(filename::String, skiprows=6)
     start = skiprows + 1
     lines = readlines(filename)
+
+    # parse line 1 for event details
     event, md, y = split(strip(lines[1]), ",")
     date = strip(md * y)
+
+    # parse line 2 for station details
     station, lat, lon, comp = match(r"\s*(\S*)\s*Lat.*Lon([\-0-9 ]+[NnSs])\s*([ \-0-9]+[EeWw])\s*Comp:\s*(.*)", lines[2])
+
+    # parse line 3
     filter = lines[3]
+
+    # parse line 4 for initial conditions
     initV = match(r"\s*Initial\s*Velocity\s*=\s*([\-\.0-9E]*)\s*(\S*)", lines[4])
     initV = initV[1] * " " * initV[2]
     initD = match(r".*Initial\s*Displacement\s*=\s*([\-\.0-9E]*)\s*(\S*)", lines[4])
     initD = initD[1] * " " * initD[2]
+
+    # parse npts, dt
     npts, aunit, dt = match(r"\s*(\d*)\s*(.*)at\s*([\.0-9]*)\s*\S*", lines[skiprows])
     npts = parse(Int64, npts)
     dt = parse(Float64, dt)
     t = dt:dt:npts*dt
+
+    # read acceleration data
     ptsPerLine = length(split(lines[start]))
-    nLines = Int(ceil(npts/(ptsPerLine+0.001)))
+    nLines = trunc(Int, npts/(ptsPerLine+0.001)) + 1
     aEnd = skiprows + nLines
     a = [parse(Float64, j) for i = start:aEnd for j in split(lines[i])]
+
+    # read velocity data
     vStart = aEnd + 3
-    vEnd = vStart + nLines - 1
+    vnpts, vunit, vdt = match(r"\s*(\d*)\s*(.*)at\s*([\.0-9]*)\s*\S*", lines[vStart-1])
+    vnLines = trunc(Int, vnpts/(ptsPerLine+0.001)) + 1
+    vEnd = vStart + vnLines - 1
     v = [parse(Float64, j) for i = vStart:vEnd for j in split(lines[i])]
+
+    # read displacement data
     dStart = vEnd + 3
-    dEnd = dStart + nLines - 1
+    dnpts, dunit, ddt = match(r"\s*(\d*)\s*(.*)at\s*([\.0-9]*)\s*\S*", lines[dStart-1])
+    dnLines = trunc(Int, dnpts/(ptsPerLine+0.001)) + 1
+    dEnd = dStart + dnLines - 1
     d = [parse(Float64, j) for i = dStart:dEnd for j in split(lines[i])]
-    npts, vunit, dt = match(r"\s*(\d*)\s*(.*)at\s*([\.0-9]*)\s*\S*", lines[vStart-1])
-    npts, dunit, dt = match(r"\s*(\d*)\s*(.*)at\s*([\.0-9]*)\s*\S*", lines[dStart-1])
+
+    # prepare data to return
     aMeta = Dict(
         "event" => event, "date" => date, "station" => station,
         "lat" => lat, "lon" => lon, "component" => comp,
         "filter" => filter, "initV" => initV, "initD" => initD,
-        "yunit" => aunit,
+        "yunit" => aunit, "npts" => npts, "dt" => dt,
     )
     vMeta = Dict(
         "event" => event, "date" => date, "station" => station,
         "lat" => lat, "lon" => lon, "component" => comp,
         "filter" => filter, "initV" => initV, "initD" => initD,
-        "yunit" => vunit,
+        "yunit" => vunit, "npts" => vnpts, "dt" => vdt,
     )
     dMeta = Dict(
         "event" => event, "date" => date, "station" => station,
         "lat" => lat, "lon" => lon, "component" => comp,
         "filter" => filter, "initV" => initV, "initD" => initD,
-        "yunit" => dunit,
+        "yunit" => dunit, "npts" => dnpts, "dt" => ddt,
     )
     return TimeSeries(t, a, aMeta), TimeSeries(t, v, vMeta), TimeSeries(t, d, dMeta)
 end
